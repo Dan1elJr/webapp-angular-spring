df37ec509a821fe29ddf2436ef140efb5713b8e5
Daniel Junior <danieljunior1903@gmail.com>
2026-08-01 12:24:26 -0400

    Enablign menus based on user auth

--- todo/src/app/menu/menu.ts
+++ todo/src/app/menu/menu.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HardCodedAuthentication } from '../service/hard-coded-authentication';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  imports: [RouterLink,NgIf],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {
+export class Menu implements OnInit {
 
-  
+    public isUserLogged: boolean = false;
+
+    constructor(public hardcodedAuthenticationService: HardCodedAuthentication) { 
+    } 
+
+    ngOnInit(): void {
+        this.isUserLogged = this.hardcodedAuthenticationService.isUserLoggedIn();
+    }
+
+
 }
