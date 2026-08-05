9362c8909d4f44ee339fdc7596cb83abfe142b5c
Daniel Junior <danieljunior1903@gmail.com>
2026-08-04 20:33:16 -0400

    Securing components and redirecting

--- todo/src/app/service/route-guard.ts
+++ todo/src/app/service/route-guard.ts
@@ -1,5 +1,5 @@
-import { Service } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Service, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { HardCodedAuthentication } from './hard-coded-authentication';
 
 
@@ -7,12 +7,15 @@ import { HardCodedAuthentication } from './hard-coded-authentication';
 @Service()
 export class RouteGuard implements CanActivate {
 
-    constructor(private hardCodedAuthentication: HardCodedAuthentication) { 
-
-    }
+    private hardCodedAuthentication = inject(HardCodedAuthentication);
+    private router = inject(Router);
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
             
-        return this.hardCodedAuthentication.loggedIn? true : false;
+        if(this.hardCodedAuthentication.loggedIn){
+            return true;
+        }
+        this.router.navigate(['login']);
+        return false;
     }
 }
