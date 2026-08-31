fbacb0bd936cef8f627a4d83fc297699783c019b
Daniel Junior <danieljunior1903@gmail.com>
2026-08-31 19:35:09 -0400

    Creating controller path to update Todo - PUT request

--- backend/src/main/java/com/sandaniel/rest/webservices/controller/TodoResource.java
+++ backend/src/main/java/com/sandaniel/rest/webservices/controller/TodoResource.java
@@ -3,11 +3,14 @@ package com.sandaniel.rest.webservices.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +49,14 @@ public class TodoResource {
 		}
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+		
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todoUpdated,HttpStatus.OK);
+		
+	}
+	
 	
 }
